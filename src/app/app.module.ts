d40c934da5e3bd6cdc432e47e8aa76c33cc27dcc
nidhiagrl <nidhiagrl257@gmail.com>
2019-06-07 22:22:06 +0530

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { CustomerComponent } from './customer/customer.component';
 import { OrderComponent } from './order/order.component';
 import { AboutComponent } from './about/about.component';
 import { LoginComponent } from './login/login.component';
+import { CustomerFormComponent } from './customer-form/customer-form.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule,
@@ -17,9 +18,10 @@ import { LoginComponent } from './login/login.component';
     {path :'customer', component:CustomerComponent},
     {path :'order', component:OrderComponent},
     {path :'about', component:AboutComponent},
-    {path :'login', component:LoginComponent}
+    {path :'login', component:LoginComponent},
+    {path:'new',component:CustomerFormComponent}
   ])],
-  declarations: [ AppComponent, HelloComponent, BsNavbarComponent, CustomerComponent, OrderComponent, AboutComponent, LoginComponent ],
+  declarations: [ AppComponent, HelloComponent, BsNavbarComponent, CustomerComponent, OrderComponent, AboutComponent, LoginComponent, CustomerFormComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
