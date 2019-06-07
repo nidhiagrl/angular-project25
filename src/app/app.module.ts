1944c1496aaa0f038d2e8157d9ac45a48f8431c9
nidhiagrl <nidhiagrl257@gmail.com>
2019-06-08 00:16:20 +0530

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
@@ -12,14 +14,25 @@ import { AboutComponent } from './about/about.component';
 import { LoginComponent } from './login/login.component';
 import { CustomerFormComponent } from './customer-form/customer-form.component';
 
+const firebaseConfig = {
+    apiKey: "AIzaSyCIemuwK1j2jFbVzSFVk-3v0xH_5Hbe7nE",
+    authDomain: "angular-stack-demo.firebaseapp.com",
+    databaseURL: "https://angular-stack-demo.firebaseio.com",
+    projectId: "angular-stack-demo",
+    storageBucket: "angular-stack-demo.appspot.com",
+    messagingSenderId: "213760179206",
+    appId: "1:213760179206:web:6f01998eb073b048"
+  };
 @NgModule({
   imports:      [ BrowserModule, FormsModule,
+  AngularFireModule.initializeApp(firebaseConfig),
+  AngularFirestoreModule,
   RouterModule.forRoot([
     {path :'customer', component:CustomerComponent},
     {path :'order', component:OrderComponent},
     {path :'about', component:AboutComponent},
     {path :'login', component:LoginComponent},
-    {path:'new',component:CustomerFormComponent}
+    {path:'customer/new',component:CustomerFormComponent}
   ])],
   declarations: [ AppComponent, HelloComponent, BsNavbarComponent, CustomerComponent, OrderComponent, AboutComponent, LoginComponent, CustomerFormComponent ],
   bootstrap:    [ AppComponent ]
