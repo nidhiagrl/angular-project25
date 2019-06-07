9d7035657357ffe6ed1f9f0dccb62b5e204c37e6
nidhiagrl <nidhiagrl257@gmail.com>
2019-06-07 18:27:21 +0530

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
+import { CustomerComponent } from './customer/customer.component';
+import { OrderComponent } from './order/order.component';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule,
+  RouterModule.forRoot([
+    {path :'customer', component:CustomerComponent},
+    {path :'order', component:OrderComponent},
+    {path :'about', component:AboutComponent},
+    {path :'login', component:LoginComponent}
+  ])],
+  declarations: [ AppComponent, HelloComponent, BsNavbarComponent, CustomerComponent, OrderComponent, AboutComponent, LoginComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
